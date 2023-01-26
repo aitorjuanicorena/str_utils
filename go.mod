5c46875b423acd34d313951f97f895def4194ff8
Aitor Juanicorena <a.juanicorena@saltosystems.com>
2023-01-26 09:29:43 +0100

    feat: Add some useful functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aitorjuanicorena/str_utils
+
+go 1.18
---
+++ trimFirstChars.go
@@ -0,0 +1,23 @@
+package str_utils
+
+import (
+	"errors"
+)
+
+func trimFirstChars(str_in string, trim_pos int) (str_out string, err error) {
+
+	err = errors.New("String size is lower than trim position")
+	str_out = str_in
+	size := len(str_in)
+
+	if size > trim_pos {
+
+		str_out = str_in[trim_pos:]
+
+		err = nil
+
+	}
+
+	return
+
+}
